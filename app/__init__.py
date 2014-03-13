6cb7f3548de3e52bbf0d8a4529a8922bad0e1257
deadbok <martin.groenholdt@gmail.com>
2014-03-14 00:21:20 +0100

    GPL licensed

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,24 @@
+'''
+Copyright 2014 Martin Grønholdt
+
+This file is part of RadioPiWeb.
+
+RadioPiWeb is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RadioPiWeb is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RadioPiWeb.  If not, see <http://www.gnu.org/licenses/>.
+'''
 from flask import Flask
 
-VERSION = '0.2'
+VERSION = '0.3'
 
 app = Flask(__name__)
 app.config.from_object('config')
